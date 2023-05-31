1c30008c24a517857d82995e1f2890c27d61f9e6
Michael Amudeshnikov <83592338+misha99fr@users.noreply.github.com>
2023-05-31 20:58:02 +0300

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -5,7 +5,7 @@ local workspace, window, menu = system.addWindow(GUI.filledWindow(1, 1, 50, 16, 
 
 local layout = window:addChild(GUI.layout(1, 1, window.width, window.height, 1, 1))
 
-local textbox = layout:addChild(GUI.input(1, 1, 25, 1, 0xFFFFFF, 0x000000, 0xC3C3C3, 0xFFFFFF, 0x000000, "", "Enter a number"))
+local textbox = layout:addChild(GUI.input(1, 1, 25, 1, 0xFFFFFF, 0x000000, 0xC3C3C3, 0xFFFFFF, 0x000000, "", "Wat do u want to order:"))
 
 local button1 = layout:addChild(GUI.button(1, 1, 8, 1, 0x4B4B4B, 0xFFFFFF, 0x0, 0xFFFFFF, "1"))
 button1.onTouch = function()
