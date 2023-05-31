97711495f0c7284a56cbc5860a9c5eeaac388179
Michael Amudeshnikov <83592338+misha99fr@users.noreply.github.com>
2023-05-31 22:04:30 +0300

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,11 +1,11 @@
 local GUI = require("GUI")
 local system = require("System")
 
-local workspace, window, menu = system.addWindow(GUI.filledWindow(1, 1, 50, 16, 0xE1E1E1))
+local workspace, window, menu = system.addWindow(GUI.filledWindow(1, 1, 50, 16, 0xFFF5180D))
 
 local layout = window:addChild(GUI.layout(1, 1, window.width, window.height, 1, 1))
 
-local textbox = layout:addChild(GUI.input(1, 1, 25, 1, 0xFFFFFF, 0x000000, 0xC3C3C3, 0xFFFFFF, 0x000000, "", "Wat do u want to order:"))
+local label = layout:addChild(GUI.label(1, 1, 25, 1, 0xFFFFFF, "Welcome to MCdonalds, What do you want to order))
 
 local button1 = layout:addChild(GUI.button(1, 1, 11, 1, 0x4B4B4B, 0xFFFFFF, 0x0, 0xFFFFFF, "Cheesburger"))
 button1.onTouch = function()
