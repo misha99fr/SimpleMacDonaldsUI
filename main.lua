f1e2e2da57c00e8ccea0f248e02aae265e0dd57a
Michael Amudeshnikov <83592338+misha99fr@users.noreply.github.com>
2023-10-31 16:09:32 +0300

    fsdfkdsa

--- main.lua
+++ main.lua
@@ -1,22 +1,20 @@
 local GUI = require("GUI")
 local system = require("System")
 
-local workspace, window, menu = system.addWindow(GUI.filledWindow(1, 1, 50, 16, 0xFFFF0000))
+local workspace, window, menu = system.addWindow(GUI.filledWindow(1, 1, 50, 16, 0xff0000))
 
 local layout = window:addChild(GUI.layout(1, 1, window.width, window.height, 1, 1))
 
-local label = layout:addChild(GUI.label(1, 1, 25, 1, 0xFFFFFF, "Welcome to MCdonalds, You want to order:"))
+local label = layout:addChild(GUI.label(1, 1, 25, 1, 0xFFFFFF, "Welcome to MCdonalds, OUR MENU:"))
 
 local button1 = layout:addChild(GUI.button(1, 1, 11, 1, 0x4B4B4B, 0xFFFFFF, 0x0, 0xFFFFFF, "Cheesburger"))
 button1.onTouch = function()
-  window:remove()
   local order = os.time()
   GUI.alert("Hello, you ordered cheesburger,\nOrder number: " .. order)
 end
 
 local button2 = layout:addChild(GUI.button(1, 1, 8, 1, 0x4B4B4B, 0xFFFFFF, 0x0, 0xFFFFFF, "Fries"))
 button2.onTouch = function()
-  window:remove()
   local order = os.time()
   GUI.alert("Hello, you ordered fries, \n Your Order number: " .. order)
 end
@@ -24,6 +22,7 @@ end
 local button3 = layout:addChild(GUI.button(1, 1, 8, 1, 0x4B4B4B, 0xFFFFFF, 0x0, 0xFFFFFF, "Exit"))
 button3.onTouch = function()
   window:remove()
+  GUI.alert("Also check OPENOS VERSION on my github: github.com/cackemc")
 end
 
 workspace:draw()
